cb584ce5a9f42b14558b1aaf0d4f2727df7e67b4
Harshal Gami <190540107063@darshan.ac.in>
2021-11-26 19:01:41 +0530

    1.0.3

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+$(document).ready(function () {
+
+    //scroll
+    $(window).scroll(function () {
+
+        //header color change transparent to white
+        if (this.scrollY > 00) {
+            $('.header1').addClass("color-header");
+        } else {
+            $('.header1').removeClass("color-header");
+        }
+        //header stage 2 hidden or show
+        if (this.scrollY > 100) {
+            $('.stage-2').addClass("show");
+        } else {
+            $('.stage-2').removeClass("show");
+        }
+    });
+})
