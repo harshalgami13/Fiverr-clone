f47eee3ccd99291237f926c00604881a324746bc
Harshal Gami <190540107063@darshan.ac.in>
2021-11-30 09:35:52 +0530

    v1.0.9

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 // Swiper
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper1", {
     spaceBetween: 30,
     effect: "fade",
     loop: true,
@@ -16,7 +16,44 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    
 });
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      650: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      }
+    },
+  });
 
 
 $(document).ready(function () {
@@ -24,7 +61,32 @@ $(document).ready(function () {
     $('.main1 .cancle img').click(function () {
         $('.main1').addClass("cancle");
     })
+    $('.head-btn').click(function () {
+        $('.side-menu').addClass("offcanvas-menu");
+        // $('.head-btn').addClass("show");
+    })
+    $('.close-btn').click(function () {
+        $('.head-btn').removeClass("show");
+        $('.side-menu').removeClass("offcanvas-menu");
+    })
+    $('.side-menu-content ul li a').click(function () {
+        $('.head-btn').removeClass("show");
+        $('.side-menu').removeClass("offcanvas-menu");
+    })
     
+
+    function checkwidth() {
+        var windowSize = $(window).width();
+        if (windowSize > 800) {
+            $('.side-menu').removeClass("offcanvas-menu");
+        }
+    }
+    checkwidth();
+    $(window).resize(checkwidth);
+
+
+
+
     //scroll
     $(window).scroll(function () {
 
@@ -41,4 +103,8 @@ $(document).ready(function () {
             $('.stage-2').removeClass("show");
         }
     });
+
+
+
+
 })
